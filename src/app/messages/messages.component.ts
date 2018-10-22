52f33d92320fa5b041dbf2778377596233f7db2f
Sandeep Kalra <BZ1347@NAM.corp.gm.com>
2018-10-22 11:54:14 -0400

    compilation warnings removed

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, Subject, ReplaySubject } from 'rxjs';
-import { map, filter, switchMap } from 'rxjs/operators';
-
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, Subject, ReplaySubject} from 'rxjs';
+import {map, filter, switchMap} from 'rxjs/operators';
 
 
 export class MessageBody {
-			title: string;
-			message:string;
-			email: string;
-			password: string;
+    title: string;
+    message: string;
+    email: string;
+    password: string;
 }
+
 export class MessageResp {
-		code: number;
-		message: string;
-		body: MessageBody[];
+    code: number;
+    message: string;
+    body: MessageBody[];
 }
 
 export class UserData {
@@ -22,35 +22,36 @@ export class UserData {
 
 
 @Component({
-		selector: 'app-messages',
-		templateUrl: './messages.component.html',
-		providers: [UserData, MessageBody],
-		styleUrls: ['./messages.component.scss']
+    selector: 'app-messages',
+    templateUrl: './messages.component.html',
+    providers: [UserData, MessageBody],
+    styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent implements OnInit {
 
-		loaded =false
-		data = MessageResp;
-		constructor(
-				private http: HttpClient, 
-				private ud: UserData) { 
-		}
+    loaded = false;
+    data:MessageResp;
 
-		postMessage(m MessageBody):observable<MessageResp> {
-				this.http.post<MessageResp>("http://10.59.1.16:8080/api/post", m)
-				.subscribe(data => this.data = data)
-		}
+    constructor(
+        private http: HttpClient,
+        private ud: UserData) {
+    }
 
-		getMessages():Observable<MessageResp> {
-				this.http.post<Message[]>("http://10.58.1.16:8080/api/read", this.ud)
-				.subscribe(
-						x => { this.data = x },
-						err => { console.error('Observer got an error: ' + err)} ,
-						() => {this.loaded = true;}
-				)
-		}
+    postMessages(m: MessageBody) {
+        this.http.post<MessageResp>(
+            "http://localhost:8080/api/post", m).subscribe(data => this.data = data)
+    }
 
-  ngOnInit() {
-  }
+    getMessages() {
+        this.http.post<MessageResp>("http://localhost:8080/api/read", this.ud)
+            .subscribe(
+                x => { console.log("getMsg - data = ", x); /* data.x = x */},
+                err => {console.error('Observer got an error: ' + err);},
+                () => {console.log("loading complete") , this.loaded = true;}
+            )
+    }
+
+    ngOnInit() {
+    }
 
 }
